be8c5ac3bc0fca9df452d63e03cf5570d1e9300a
nputrautama <nyoman.putra@ruangguru.com>
2022-07-11 20:09:13 +0800

    feat: update phase 4

--- shared_libraries/component/lib/widget/text_field/custom_text_field.dart
+++ shared_libraries/component/lib/widget/text_field/custom_text_field.dart
@@ -1,3 +1,6 @@
+import 'package:component/widget/text_field/bloc/custom_text_field_cubit.dart';
+import 'package:component/widget/text_field/bloc/custom_text_field_state.dart';
+import 'package:dependencies/bloc/bloc.dart';
 import 'package:dependencies/flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/material.dart';
 import 'package:resources/colors.gen.dart';
@@ -26,51 +29,84 @@ class CustomTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          labelText,
-          style: TextStyle(
-            color: ColorName.textDarkBlue,
-            fontSize: 14.sp,
-            fontWeight: FontWeight.w400,
-          ),
-        ),
-        SizedBox(
-          height: 3.h,
-        ),
-        TextField(
-          controller: controller,
-          keyboardType: textInputType,
-          obscureText: obscureText,
-          onChanged: onChanged,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(1.5),
-              borderSide: const BorderSide(
-                width: 0,
-                style: BorderStyle.none,
-              ),
-            ),
-            contentPadding: const EdgeInsets.symmetric(
-              horizontal: 13.0,
-              vertical: 10.0,
-            ),
-            errorText: errorText,
-            hintText: hintText,
-            hintStyle: TextStyle(
-              color: ColorName.textFieldHintGrey,
-              fontSize: 16.sp,
-              fontWeight: FontWeight.w400,
-            ),
-            hoverColor: ColorName.orange,
-            focusColor: ColorName.orange,
-            filled: true,
-            fillColor: ColorName.textFieldBackgroundGrey,
-          ),
-        ),
-      ],
+    return BlocProvider(
+      create: (_) => CustomTextFieldCubit(),
+      child: BlocBuilder<CustomTextFieldCubit, CustomTextFieldState>(
+          builder: (context, state) {
+            final bool isSecure = state.customTextFieldState.data ?? obscureText;
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  labelText,
+                  style: TextStyle(
+                    color: ColorName.textDarkBlue,
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(
+                  height: 3.h,
+                ),
+                Container(
+                  color: ColorName.textFieldBackgroundGrey,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: controller,
+                          keyboardType: textInputType,
+                          obscureText: obscureText ? isSecure : obscureText,
+                          onChanged: onChanged,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(1.5),
+                              borderSide: const BorderSide(
+                                width: 0,
+                                style: BorderStyle.none,
+                              ),
+                            ),
+                            contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 13.0,
+                              vertical: 10.0,
+                            ),
+                            hintText: hintText,
+                            hintStyle: TextStyle(
+                              color: ColorName.textFieldHintGrey,
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w400,
+                            ),
+                            hoverColor: ColorName.orange,
+                            focusColor: ColorName.orange,
+                            filled: true,
+                            fillColor: ColorName.textFieldBackgroundGrey,
+                          ),
+                        ),
+                      ),
+                      if (obscureText)
+                        IconButton(
+                          splashColor: Colors.transparent,
+                          highlightColor: Colors.transparent,
+                          onPressed: () =>
+                              context.read<CustomTextFieldCubit>().isSecureText(
+                                isSecured: isSecure,
+                              ),
+                          icon: Icon(
+                            isSecure ? Icons.remove_red_eye : Icons.visibility_off,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+                Text(
+                  errorText ?? "",
+                  style: const TextStyle(color: Colors.red),
+                ),
+              ],
+            );
+          }),
     );
   }
 }
