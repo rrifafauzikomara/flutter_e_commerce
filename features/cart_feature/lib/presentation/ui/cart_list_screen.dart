f7a0a826cabaf1b1423f0fd7acff3e66ebee9c5b
rrifafauzikomara <rifafauzi6@gmail.com>
2022-07-17 20:36:15 +0700

    feat: wiring cart data to UI

--- features/cart_feature/lib/presentation/ui/cart_list_screen.dart
+++ features/cart_feature/lib/presentation/ui/cart_list_screen.dart
@@ -1,9 +1,15 @@
-import 'package:cart_feature/presentation/bloc/cart_cubit.dart';
+import 'package:cart_feature/presentation/bloc/bloc.dart';
+import 'package:common/utils/state/view_data_state.dart';
+import 'package:component/widget/card/cart_card.dart';
+import 'package:component/widget/check_box/custom_check_box.dart';
 import 'package:dependencies/bloc/bloc.dart';
 import 'package:dependencies/flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/material.dart';
 import 'package:resources/assets.gen.dart';
 import 'package:resources/colors.gen.dart';
+import 'package:common/utils/extensions/money_extension.dart';
+import 'package:component/widget/progress_indicator/custom_circular_progress_indicator.dart';
+import 'package:component/widget/button/custom_button.dart';
 
 class CartListScreen extends StatefulWidget {
   const CartListScreen({Key? key}) : super(key: key);
@@ -58,6 +64,139 @@ class _CartListScreenState extends State<CartListScreen> {
           ],
         ),
         iconTheme: const IconThemeData(color: ColorName.orange),
+      ),
+      body: Center(
+        child: BlocConsumer<CartCubit, CartState>(
+          listener: (context, state) {},
+          builder: (context, state) {
+            if (state.cartListState.status.isLoading) {
+              return const CustomCircularProgressIndicator();
+            } else if (state.cartListState.status.isError) {
+              return Text(state.cartListState.failure!.errorMessage);
+            } else if (state.cartListState.status.isNoData) {
+              return Text(state.cartListState.message);
+            } else if (state.cartListState.status.isHasData) {
+              final total = state.cartListState.data?.amount ?? 0;
+              final products = state.cartListState.data?.product ?? [];
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const _Divider(),
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 16.h,
+                      vertical: 8.h,
+                    ),
+                    child: Row(
+                      children: [
+                        CustomCheckBox(
+                          value: false,
+                          onChanged: (bool? value) {},
+                        ),
+                        SizedBox(width: 6.w),
+                        const Text("Pilih Semua"),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      padding: EdgeInsets.zero,
+                      itemCount: products.length,
+                      itemBuilder: (context, index) {
+                        final cart = products[index];
+                        return CartCard(
+                          cart: cart,
+                          value: true,
+                          onChanged: (bool? value) {},
+                        );
+                      },
+                    ),
+                  ),
+                  _Payment(total: total),
+                ],
+              );
+            } else {
+              return const SizedBox();
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _Divider extends StatelessWidget {
+  const _Divider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: ColorName.textFieldBackgroundGrey,
+      width: double.infinity,
+      height: 1.h,
+    );
+  }
+}
+
+class _Payment extends StatelessWidget {
+  final int total;
+
+  const _Payment({
+    Key? key,
+    required this.total,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            color: ColorName.textFieldBackgroundGrey,
+            width: double.infinity,
+            height: 1.h,
+          ),
+          Container(
+            color: ColorName.white,
+            padding: EdgeInsets.only(
+                top: 10.h, bottom: 8.h, left: 15.h, right: 15.h),
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Total Harga",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 8.sp,
+                        color: ColorName.textDarkGrey,
+                      ),
+                    ),
+                    Text(
+                      total.toIDR(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 10.sp,
+                        color: ColorName.orange,
+                      ),
+                    )
+                  ],
+                ),
+                const Spacer(),
+                SizedBox(
+                  width: 123.w,
+                  child: CustomButton(
+                    buttonText: "Beli",
+                    onTap: () {},
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ shared_libraries/component/lib/widget/card/cart_card.dart
@@ -0,0 +1,186 @@
+import 'package:cart/domain/entity/response/chart_entity.dart';
+import 'package:component/widget/check_box/custom_check_box.dart';
+import 'package:component/widget/progress_indicator/custom_circular_progress_indicator.dart';
+import 'package:dependencies/cached_network_image/cached_network_image.dart';
+import 'package:dependencies/flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/material.dart';
+import 'package:resources/assets.gen.dart';
+import 'package:resources/colors.gen.dart';
+import 'package:common/utils/extensions/money_extension.dart';
+
+class CartCard extends StatelessWidget {
+  final ProductsChartEntity cart;
+  final bool value;
+  final ValueChanged<bool?>? onChanged;
+
+  const CartCard({
+    Key? key,
+    required this.cart,
+    required this.value,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const _Divider(),
+        Container(
+          margin: EdgeInsets.only(
+            top: 21.h,
+            bottom: 18.h,
+            left: 16.h,
+            right: 16.h,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                cart.product.userInfo.name,
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 10.sp,
+                  color: ColorName.textDarkGrey,
+                ),
+              ),
+              Text(
+                cart.product.userInfo.city,
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 8.sp,
+                  color: ColorName.textDarkGrey,
+                ),
+              ),
+              SizedBox(height: 14.h),
+              Row(
+                children: [
+                  CustomCheckBox(
+                    value: value,
+                    onChanged: onChanged,
+                  ),
+                  SizedBox(width: 6.w),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(12.0),
+                              ),
+                              child: CachedNetworkImage(
+                                height: 64.w,
+                                width: 64.w,
+                                imageUrl: cart.product.imageUrl,
+                                placeholder: (context, url) => const Center(
+                                    child: CustomCircularProgressIndicator()),
+                                errorWidget: (context, url, error) =>
+                                    const Center(child: Icon(Icons.error)),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            SizedBox(width: 8.w),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    cart.product.name,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 10.sp,
+                                      color: ColorName.textDarkGrey,
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.h),
+                                  Text(
+                                    cart.product.price.toIDR(),
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 10.sp,
+                                      color: ColorName.orange,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 8.w),
+                          height: 35.h,
+                          width: double.infinity,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(1.5),
+                                borderSide: const BorderSide(
+                                  width: 0,
+                                  style: BorderStyle.none,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 13.0,
+                                vertical: 5.0,
+                              ),
+                              hintText: "Catatan Untuk Toko",
+                              hintStyle: TextStyle(
+                                color: ColorName.textFieldHintGrey,
+                                fontSize: 16.sp,
+                                fontWeight: FontWeight.w400,
+                              ),
+                              hoverColor: ColorName.orange,
+                              focusColor: ColorName.orange,
+                              filled: true,
+                              fillColor: ColorName.textFieldBackgroundGrey,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 12.h),
+              Row(
+                children: [
+                  const Spacer(),
+                  Assets.images.icon.cart.svg(width: 12.w, height: 12.h),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.add),
+                  ),
+                  Text(cart.quantity.toString()),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.add,
+                      color: ColorName.orange,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        const _Divider(),
+      ],
+    );
+  }
+}
+
+class _Divider extends StatelessWidget {
+  const _Divider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: ColorName.textFieldBackgroundGrey,
+      width: double.infinity,
+      height: 1.h,
+    );
+  }
+}
